a59b2ab8ed5517918e376d6b723be73e51eaf6fa
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2023-02-15 22:18:29 +0300

    V10 liquibase correction

--- src/main/resources/liquibase/scripts/student.sql
+++ src/main/resources/liquibase/scripts/student.sql
@@ -1,13 +1,5 @@
 --liquibase formatted sql
 
---changeSet agerb:1
-CREATE TABLE IF NOT EXISTS public.student
-(
-    id_student bigint NOT NULL PRIMARY KEY,
-    age integer NOT NULL DEFAULT 20,
-    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    faculty_id bigint
-    )
 --changeSet agerb:2
 CREATE INDEX student_by_idx_name ON student (name);
 
